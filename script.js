50b9859c4991d52d5aabdc79c4f277bb2e9a70f9
arotto8719 <arotto87@gmail.com>
2019-10-18 21:34:23 -0500

    button update

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   // function print("https://handlers.education.launchcode.org/static/planets.json") {
-   //    };
+   let form = document.querySelector("form");
       fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-
-         response.json().then(function(json) {
+         console.log(response);
+         // response.json().then(function(json) {
        const div = document.getElementById('missionTarget');
        // Add HTML that includes the JSON Data
             div.innerHTML = `
                            <h2>Mission Destination</h2>
                            <ol>
-                              <li>Name: ${"Saturn/Titan"}</li>
-                              <li>Diameter: ${"5149.5 km"}</li>
-                              <li>Star: ${"Sol"}</li>
-                              <li>Distance from Earth: ${"1.4 billion km from Earth"}</li>
-                              <li>Number of Moons: ${0}</li>
+                              <li>Name: ${json.name}</li>
+                              <li>Diameter: ${json.diameter}</li>
+                              <li>Star: ${json.star}</li>
+                              <li>Distance from Earth: ${json.distancefromEarth}</li>
+                              <li>Number of Moons: ${json.numberofMoons}</li>
                            </ol> 
                            <img src="${"https://solarsystem.nasa.gov/system/resources/detail_files/16278_PIA20016.jpg"}">
                          `;         
-         // console.log(json);
-         });
+            // console.log(json);
+         // });
       });
    console.log('window loaded');
 
 function init() {
       let form = document.getElementById("launchform");
-      form.addEventListener("submit", function() { 
+      let button = document.getElementById("submit");
+         form.addEventListener("submit", function() {  
+         button.addEventListener("submit", function (event) {
+         document.getElementById('submitbutton').enabled = !cansubmit;
          event.preventDefault();
 
          let pilotStatus = document.getElementById("pilotStatus");
@@ -45,12 +47,12 @@ function init() {
                alert("all fields are required!");  
            }
          if(isNaN(cargoWeight.value) || isNaN(fuelLevel.value)) {
-               alert("Make sure to enter valid information for each field!");              
+               alert("Make sure to enter valid information for each field!");             
            }
          if(Number.isInteger(pilotName.value) || Number.isInteger(copilotName.value)) {
               alert("Make sure to enter valid information for each field!");
            }
-           
+
          if (fuelLevel.value < 10000) {
             faultyItems.style.visibility = "visible";
             launchStatusCheck = false;
@@ -73,43 +75,21 @@ function init() {
          //    fuelStatus.inner.HTML = "not enough fuel for the journey"
          //    launchStatus.inner.HTML = "Shuttle not ready for launch"
          //    launchStatus.attr("id", "launchStatusInvalid"); -890 liters of fuel part
-         // }   
-   
-         console.log("alert");
-         
-   });    
-      
-//             let div = document.getElementById("missionTarget");
-//             
-//     
-//              
-               
-                   
-//                    {/* <h3>Email: <a href="sendto:${json[i].html}">${json[i].html}</a></h3>
-//                    <p><strong>Container:</strong> ${json[i].body}</p> */}
-                   
-//             }
-//          });
-//       // });
-//   });
-// }
- // fetch(url).then(function(response)) {
-        //     <!-- JSON data?   -->
-        //   })
-        //   catch.function (err) {
-        //   });
-        //   
-        //   let planet = json[2];
-        //         console.log(typeof div);
-        //         div["className"] = "planet";
-        //         div.innerHTML = 
-        //           <li>Diameter: ${"5149.5 km"}</li>
-        //           <li>Star: ${"Sol"}</li>
-        //           <li>Distance from Earth: ${1.4 billion km from Earth"}</li>
-        //           <li>Number of Moons: ${0}</li>
-        //        </ol>
-        // <img src="${"https://solarsystem.nasa.gov/system/resources/detail_files/16278_PIA20016.jpg"}">
-}
+         // }  
+         console.log("alert"); 
+
+      function submitButtonClicked() {
+         document.getElementById("submitButton").innerHTML += "submitButtonClicked...";
+
+         if (launchStatusValid === true) {
+         document.getElementById('submitButton').enabled = true;
+        }
+    } 
+      document.getElementById("form").submit(); 
+      button.addEventListener("click", submitButton);
+         console.log("submit button clicked");
+         }); 
+   });
 window.onload = init;
 });
    /* This block of code shows how to format the HTML once you fetch some planetary JSON! */
